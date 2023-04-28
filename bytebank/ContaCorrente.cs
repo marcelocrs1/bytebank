e1e00776d7476d14a35b88635a86d5abde1a5296
Marcelo Santos <marcelocrs@gmail.com>
2023-04-29 00:14:53 +0100

    Aula entendo tipos concluida

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -14,5 +14,37 @@ namespace bytebank
         public double saldo;
         public string moeda;
 
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+        }
+
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -7,7 +7,41 @@ contaDoAndre.conta = "1010-X";
 contaDoAndre.saldo = 100;
 contaDoAndre.moeda = "R$";
 
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Luiza";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+contaDaMaria.moeda = "R$";
+
+//Saldo inicial contas
 Console.WriteLine("Saldo da conta do André = " + contaDoAndre.moeda + contaDoAndre.saldo);
+Console.WriteLine("Saldo da conta da Maria = " + contaDaMaria.moeda + contaDaMaria.saldo);
+
+//Movimentos do André
+
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo da conta do André pós-deposito = " + contaDoAndre.moeda + contaDoAndre.saldo);
+
+if (contaDoAndre.Sacar(300) == true)
+{
+    Console.WriteLine("Saldo da conta do André pós-saque =" + contaDoAndre.saldo);
+}
+else
+{
+    Console.WriteLine("Saldo insuficiente para saque.");
+}
+
+contaDoAndre.Transferir(50, contaDaMaria);
+Console.WriteLine("Saldo da conta do André pós-transferência = " + contaDoAndre.moeda + contaDoAndre.saldo);
+Console.WriteLine("Saldo da conta da Maria pós-transferência Recebida = " + contaDaMaria.moeda + contaDaMaria.saldo);
+
+
+
+//Movimentos da Maria
+
 
 Console.WriteLine("Tecle enter para fechar ...");
 Console.ReadLine();
